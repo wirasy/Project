57044468216f62399e8ab91d43451b4cef07f68d
Ndraaa1101 <indragamteng3@gmail.com>
2024-05-22 08:23:41 +0700

    dfbb

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <div class="wrapper">
+            <div class="logo">heheha</div>
+            <div class="menu">
+                <ul>
+                    <li><a href="">1</a></li>
+                    <li><a href="">1</a></li>
+                    <li><a href="">1</a></li>
+                    <li><a href="">1</a></li>
+                    <li><a href="">1</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</body>
+</html>
